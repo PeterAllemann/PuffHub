12bf7861187c320505eb1dc1eab76461e1fc3802
PeterAllemann <peter.allemann@students.unibe.ch>
2019-04-18 21:33:53 +0200

    You know what power is? Thats power.

--- Task3/Croping/Cutter.py
+++ Task3/Croping/Cutter.py
@@ -12,36 +12,29 @@ imArray = numpy.asarray(im)
 paths = [0]*15
 attributes = [0]*15
 
-#pathx, attributex = svg.svg2paths('../ground-truth/locations/270.svg')
-#print(len(pathx[0]))
-
-'''
-for i in range(1):
+# Get trainng paths
+for i in range(10):
     paths[i], attributes[i] = svg.svg2paths('../ground-truth/locations/27' + str(i) + '.svg')
-print(paths[0][0][0][0].imag)
-'''
 
+# Get test paths
+for i in range(10,15):
+    paths[i], attributes[i] = svg.svg2paths('../ground-truth/locations/30' + str(i-10) + '.svg')
+
+# convert for conveniance or how ever this should be spelled
 polygons = []
-
-for i in range(len(paths)):
-    for j in range(len(paths[i][0])):
-        for k in range (len(paths[i][0][j])):
-            polygons.append((paths[i][0][j][k].real, paths[i][0][j][k].imag))
-print(polygons[0])
-
-# Path length
-# print(len(paths[0]))
-# Path id
-# print(attributes[0]['id'])
-# Path x coord
-# print(paths[0][0][0].real)
-# Path y coord
-# print(paths[0][0][0].imag)
+ids = []
+for h in range(len(paths)): # File 270 to 304
+#    for h in range(len(paths[i])) # number of words per image
+    for i in range(len(paths[h])): # number of words per image
+        polygon = []
+        for j in range(len(paths[h][i])): # length of each path/polygon
+            polygon.append((paths[h][i][j][0].real, paths[h][i][j][0].imag))
+        polygons.append(polygon)
+        ids.append(attributes[h][i]['id'])
 
 # create mask
-polygon = [(1092.00, 228.00), (1112.00, 227.00), (1432.00, 227.00), (1452.00, 226.00), (1575.00, 226.00), (1573.50, 147.00), (1412.00, 147.00), (1392.00, 146.00), (1152.00, 146.00), (1132.64, 141.16), (1131.45, 141.14), (1112.00, 146.00), (1034.00, 146.00), (1002.00, 228.00)]
 maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
-ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
+ImageDraw.Draw(maskIm).polygon(polygons[0], outline=1, fill=1)
 mask = numpy.array(maskIm)
 
 # assemble new image (uint8: 0-255)
@@ -52,7 +45,6 @@ newImArray[:,:,:3] = imArray[:,:,:3]
 
 # transparency (4th column)
 newImArray[:,:,3] = mask*255
-
 
 '''
 for x in range(newImArray.shape[0]):
@@ -66,4 +58,4 @@ for x in range(newImArray.shape[0]):
 # back to Image from numpy
 newIm = Image.fromarray(newImArray, "RGBA")
 newIm = newIm.crop(newIm.getbbox())
-newIm.save("out.png")
+newIm.save("../Cropped-images/out.png")
