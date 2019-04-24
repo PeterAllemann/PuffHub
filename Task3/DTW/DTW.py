05653d39f842580630f8a019eefc87a0fdff6f06
ig <luca.althaus@lstudents.unibe.ch>
2019-04-24 13:58:54 +0200

    simple dtw

---
+++ Task3/DTW/DTW.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def dtw(v1,v2):
+
+
+    mat = np.zeros(len(v1) + 1 ,len(v2) + 1)
+
+    mat[0,0] = abs(v1[0] - v2[0])
+
+
+    for i in range(1, len(v1) - 1):
+        v20 =  v2[0]
+        mat[i,0] =  abs(v1[i] - v20 ) + mat[i-1 , 0]
+
+
+
+    for j in range(1, len(v2) - 1):
+        v10 = v1[0]
+        mat[0, j] = abs(v10 - v2[j]) + mat[0 , j-1]
+
+
+
+    for i  in range(1,len(v1)-1):
+        for j  in range(1,len(v2)-1):
+            mat[i,j] = abs(v1[i] - v2[j]) + min(mat[i-1,j] , mat[i,j-1] ,mat[i-1,j-1] )
+
+
+
+    return mat[len(v1)-1 , len(v2)-1]
